dcdbb6c54b48af9a7281548e803a0367dc7ae862
lakrouehamza <lakrounehamza10@gmail.com>
2025-01-14 12:11:13 +0100

    crée des tables student,teacher,admin

--- modeling/script.sql
+++ modeling/script.sql
@@ -1,10 +1,26 @@
 create database youdemy 
 use youdemy 
 create table if not exists utilisateur (
-    id int primary key auto_incriment,
+    id int primary key auto_increment,
     name varchar(20) not null,
     firstName  varchar(20) not null,
     email varchar(20) not null,
     phone varchar(20) not null,
-    password:  varchar(100) not null
-)
+    role enum('admin','teacher','student'),
+    password:  varchar(100) not null,
+    date_incribt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+create table if not exists student (
+    id int primary key,
+    ban inyint default 0,
+    constraint FK_student  foreign key(id) references  utilisateur(id) 
+);
+create table if not exists  teacher(
+    id int primary key,
+    ban inyint default 0,
+    constraint FK_teacher  foreign key(id) references  utilisateur(id) 
+);
+create table if not exists  admin(
+    id int primary key,
+    constraint FK_admin  foreign key(id) references  utilisateur(id) 
+);
