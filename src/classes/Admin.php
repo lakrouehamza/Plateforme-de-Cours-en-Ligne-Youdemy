3427fe7196bccdac070e93e01ea06a47b7affac7
lakrounehamza <lakrounehamza10@gmail.com>
2025-01-17 00:16:08 +0100

    ajoute une méthode pou cree une Tages

--- src/classes/Admin.php
+++ src/classes/Admin.php
@@ -1,11 +1,21 @@
 <?php
 require_once("Category.php");   
+require_once("User.php"); 
+require_once("Tages.php");  
 class Admin extends User {
     public function createCategory($category){
         if($category instanceof Category){
             $connect = new Connect();
             $name =  $category->getName();
             $stmt =  $connect->getConnect()->prepare("insert into  category (name) values(:name);");
+            $stmt->bindParam(":name",$name,PDO::PARAM_STR);
+        }
+    }
+    public function createTages($tages){
+        if($tages instanceof Tages){
+            $connect =  new Connect();
+            $name =  $tages->getName();
+            $stmt =  $connect->getConnect()->prepare("insert into  tages (name) values(:name);");
             $stmt->bindParam(":name",$name,PDO::PARAM_STR);
         }
     }
