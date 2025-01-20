7e91ebebf32cc275c25d73d011c212bddd7a4699
lakrounehamza <lakrounehamza10@gmail.com>
2025-01-20 16:08:42 +0100

    add method getDonnesCours

--- src/classes/Category.php
+++ src/classes/Category.php
@@ -1,7 +1,7 @@
 <?php 
 class Category {
     private ?int $id;
-    private ?String $name;
+    private ?String $name; 
     private ?String $date;
     private ?String $image;
     private ?String $description;
